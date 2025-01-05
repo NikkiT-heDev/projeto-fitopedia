c23ddf8c690e5155d08acdd4f729fbcecae24333
NikkiT-heDev <nikkithesillydev@gmail.com>
2025-01-04 21:39:07 -0300

    Atualização: Tema Escuro PT1

--- js/script.js
+++ js/script.js
@@ -33,6 +33,40 @@ var direcaoTema = ""
 var inverterCor = ""
 var ativoTema = false
 
+var homeInicial = document.querySelector("#home-inicial")
+var body = document.body
+var heade = document.querySelector("header")
+
+function temaClaro(){
+    var inverter0 = document.querySelectorAll(".inverter0")
+    var claroVerde1 = document.querySelectorAll(".claroVerde1")
+    var bordaPreta = document.querySelectorAll(".bordaPreta")
+    var corPreta = document.querySelectorAll(".preto")
+
+    claroVerde1.forEach(elemento => {elemento.style.color = "#2b6200"});
+    bordaPreta.forEach(elemento => {elemento.style.borderColor = "#000000"})
+    inverter0.forEach(elemento => {elemento.style.filter = "invert(0%)"})
+    corPreta.forEach(elemento => {elemento.style.color = "#000000"})
+
+    body.style.backgroundColor = "#FFFFFF"
+    heade.style.backgroundImage = "linear-gradient(to left, #99ed00,#408602)"
+}
+
+function temaEscuro(){
+    var inverter100 = document.querySelectorAll(".inverter100")
+    var escuroVerde1 = document.querySelectorAll(".escuroVerde1")
+    var bordaBranca = document.querySelectorAll(".bordaBranca")
+    var corBranca = document.querySelectorAll(".branco")
+
+    escuroVerde1.forEach(elemento => {elemento.style.color = "#85e838"});
+    bordaBranca.forEach(elemento => {elemento.style.borderColor = "#ffffff"})
+    inverter100.forEach(elemento => {elemento.style.filter = "invert(100%)"})
+    corBranca.forEach(elemento => {elemento.style.color = "#ffffff"})
+
+    body.style.backgroundColor = "#272924" 
+    heade.style.backgroundImage = "linear-gradient(to right, #99ed00,#408602)"
+}
+
 function alterarTema(){
     if(!ativoTema){
         ativoTema = true
@@ -44,11 +78,13 @@ function alterarTema(){
             novoTema = "lua"
             inverterCor = "100%"
             divTemaAtual.style.marginLeft = "30px"
+            temaEscuro()
         }else{
             novoTema = "sol"
             direcaoTema= "left"
             inverterCor = "0%"
             divTemaAtual.style.marginRight = "30px"
+            temaClaro()
         }
         divTemaAtual.style.backgroundImage = `url(../imgs/icons/${novoTema}.png)`
         divTema.style.filter = `invert(${inverterCor})`
@@ -61,3 +97,4 @@ function alterarTema(){
         //Altera o icone e propriedades da barra do Tema
     }
 }
+
